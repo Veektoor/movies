4a5fa941d34e80c12f3c6713d778c66de733e98d
Veektoor <gichuhivictorm@gmail.com>
2026-04-12 12:54:28 +0300

    updated ui and fixed err

--- client/src/components/common/Topbar.jsx
+++ client/src/components/common/Topbar.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import MenuIcon from "@mui/icons-material/Menu";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
-import { AppBar, Box, Button, IconButton, Stack, Toolbar, useScrollTrigger, Typography } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Stack, Toolbar, useScrollTrigger } from "@mui/material";
 import { cloneElement, useState } from "react";
 import { Link } from "react-router-dom";
 import menuConfigs from "../../configs/menu.configs";
